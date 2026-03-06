bd5ceaf7f2dd9bd7d303134a72e72724ca5eb0ab
tiedun <3578283292@qq.com>
2026-03-06 18:42:52 +0800

    纯净项目，移除大部分无关文件

--- Qwen3-tts_base-clone.py
+++ Qwen3-tts_base-clone.py
@@ -5,8 +5,7 @@ import numpy as np
 
 # Load the model
 model = Qwen3TTSModel.from_pretrained(
-    # "/home/tiedun/models/qwen3-tts-base",
-    "/root/autodl-tmp/models/Qwen3-TTS-12Hz-1.7B-Base",
+    "/home/tiedun/models/Qwen3-TTS-12Hz-1.7B-Base",
     device_map="cuda:0",
     dtype=torch.bfloat16,
     attn_implementation="flash_attention_2",
@@ -20,12 +19,6 @@ ref_text  = """
             可以暴露名字了啊。它虽然说是一个外包，但是给的这个薪资待遇还可以啊。
             24届的话是能给到10~12K，23届呢能给到12~14K。
             """
-
-            # """
-            # 哈喽 大家好 今天再来给大家带来几个这个内推啊。首先第一个呢是一个嵌入式 嗯 汇川的一个内推，可以暴露名字了啊 它虽然说是一个外包 但是给的这个薪资待遇还可以啊 24届的话是能给到10~12K，23届呢是能给到12~14K。
-            # """
-
-# 额……对应的如果是25届呢 说的是暂不考虑，但是HR也说了就是 可以优化一下学历 那这个呢也是有操作空间的 而且是不进行任何背调的 啊 所以只要你足够优秀那这就可以啊 它这个工作地点呢目前是苏州，如果要感兴趣同学呢 可以直接发简历 啊 给我就可以了啊 嗯 谢谢各位
 
 text_long = """
             AI大模型会不会取代程序员？
@@ -49,45 +42,17 @@ text_long = """
             否则，潮水退去，你才发现自己在裸泳——那就尴尬了。
             """
 
-            # """
-            # AI大模型会不会取代程序员？
-            # 眼下，AI大模型在国内外如火如荼，有人说，程序员搞出来大模型，终将自我反噬，最先被取代。对吗？显然是笑话。
-            # 程序员将会是AGI时代最后被淘汰的一批人。程序开发是一个智力密集型产业，可以说是目前人类社会中对智力要求最高的工作之一了。
-            # 这波AI大潮，好比是蒸汽机的出现、互联网技术的出现，逐步让一批人失业，但是也创造了更多的就业机会。
-            # 一方面，AI的出现，会大大提高程序员的门槛，淘汰掉很多低端程序员。
-            # 另一方面，程序员的核心变成了怎么用AI 。
-            # 掌握先进工具的人不仅不会被AI取代，而且“升职加薪不是梦”。
-            # 所以，与其天天担心，不如早日拥抱AI。否则，潮水过后，发现自己在裸泳，那就尴尬了。
-            # """
-
 text_short = """
             各位小伙伴大家好，我是数字人 大海哥，欢迎大家收藏加关注
             """
-
-# Generate speech
-# wavs, sr = model.generate_voice_clone(
-#     text=text_long,
-#     language="Chinese",
-#     ref_audio=ref_audio,
-#     ref_text=ref_text,
-# )
 
 wavs, sr = model.generate_voice_clone(
     text=text_long,
     language="Chinese",
     ref_audio=ref_audio,
     ref_text=ref_text,
-    # 关键：限制长度 + 抑制重复
-    # max_new_tokens=4096,        # 先别放太大（按你文本长度再调）
-    repetition_penalty=1.21,    # 1.10~1.25 常用
-    # do_sample=True,
-    # temperature=0.8,            # 0.7~0.95
-    # top_p=0.95,                 # 0.9~1.0
-    # top_k=40,                   # 20~50
+    repetition_penalty=1.21,    # 重复惩罚，1.10~1.25 常用
 )
-
-# Save the resulting audio
-# sf.write("output_voice_clone.wav", wavs[0], sr)
 
 audio = wavs[0]
 if hasattr(audio, "detach"):
